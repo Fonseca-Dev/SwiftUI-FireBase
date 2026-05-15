0193b6784ec7800869ae0d8319605780c1e5c58e
kaue.fonseca <kaue.fonseca@agi.com.br>
2026-05-15 10:34:17 -0300

    Initial Commit

---
+++ iChat/iChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  iChatApp.swift
+//  iChat
+//
+//  Created by Kaue Rocha da Fonseca on 15/05/26.
+//
+
+import SwiftUI
+
+@main
+struct iChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
